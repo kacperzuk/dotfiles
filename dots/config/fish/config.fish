c4ef8e9a57084b3963b7f4113f490852ae1d3d58
Kacper Żuk <kacper.b.zuk@gmail.com>
2015-04-01 00:40:07 +0200

    Update

--- dots/config/fish/config.fish
+++ dots/config/fish/config.fish
@@ -17,5 +17,6 @@ set -x ABSROOT /home/kaz/PKGBUILDs
 set -x PATH ~/bin (find ~/Qt/Tools -type d -name bin) /home/kaz/.gem/ruby/2.2.0/bin $PATH /home/kaz/Android/android-sdk-linux/tools /home/kaz/Android/android-sdk-linux/platform-tools /home/kaz/Android/android-ndk-r10d/ ~/npm/bin ./
 set -x OPENCV_PATH ~/Android/OpenCV
 set -x EDITOR vim
+set -x ANSIBLE_HOSTS ~/Programowanie/faucetbox.com/ansible/ansible_hosts
 
 set fish_greeting
