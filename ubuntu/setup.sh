6940b030bf9f5ba2bc7da205f558f425441e9c0f
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-03-09 13:16:45 +0100

    Bugfix

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -16,11 +16,8 @@ sudo apt install -y \
     xterm
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --config vim
 sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-sudo update-alternatives --config editor
 
 sudo chsh -s /usr/bin/fish $USER
 
