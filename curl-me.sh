ed433d5b0a22f1723bce9523470f20e77cbdde12
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-03-09 13:41:09 +0100

    Interactive questions sooner

--- curl-me.sh
+++ curl-me.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+clear
+
+if [[ -e dotfiles ]]; then
+    if [[ -z "$OVERWRITE_DOTFILES" ]]; then
+        read -p "~/dotfiles already exists. Should it be removed and cloned again? [Y/n] " OVERWRITE_DOTFILES </dev/tty
+    fi
+else
+    OVERWRITE_DOTFILES=y
+fi
+
+if [[ -z "$FULL_SETUP" ]]; then
+    read -p "Full setup? [y/N] " FULL_SETUP </dev/tty
+fi
+
 if ! hash git &>/dev/null; then
 
     hash pacman &>/dev/null && sudo pacman -Sy git --noconfirm
@@ -11,26 +25,12 @@ fi
 
 cd $HOME
 
-clear
-
-if [[ -e dotfiles ]]; then
-    if [[ -z "$OVERWRITE_DOTFILES" ]]; then
-        read -p "~/dotfiles already exists. Should it be removed and cloned again? [Y/n] " OVERWRITE_DOTFILES </dev/tty
-    fi
-else
-    OVERWRITE_DOTFILES=y
-fi
-
 if [[ "$OVERWRITE_DOTFILES" != "n" ]]; then
     rm -rf dotfiles
     git clone -q https://bitbucket.org/Kazuldur/dotfiles.git
 fi
 
 cd dotfiles
-
-if [[ -z "$FULL_SETUP" ]]; then
-    read -p "Full setup? [y/N] " FULL_SETUP </dev/tty
-fi
 
 if [[ "$FULL_SETUP" == "y" ]]; then
     ./setup.sh --full
