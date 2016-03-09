d6347d10585bdb72488f599651e45886c2523020
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-03-09 12:56:01 +0100

    Bugfix

--- generic/dots/config/fish/config.fish
+++ generic/dots/config/fish/config.fish
@@ -16,8 +16,7 @@ if status --is-interactive
 end
 
 set -x ABSROOT /home/kaz/PKGBUILDs
-set -x PATH ~/bin (find ~/Qt/Tools -type d -name bin) /home/kaz/.gem/ruby/2.2.0/bin $PATH /home/kaz/Android/android-sdk-linux/tools /home/kaz/Android/android-sdk-linux/platform-tools /home/kaz/Android/android-ndk-r10d/ ~/npm/bin ~/.cabal/bin ~/.composer/vendor/bin
-set -x OPENCV_PATH ~/Android/OpenCV
+set -x PATH ~/bin $PATH ~/npm/bin
 set -x EDITOR vim
 set fish_greeting
 
