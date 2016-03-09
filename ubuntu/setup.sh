d41339751fa23d55c6231ac179cf0b5f40d3e72a
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-03-09 13:09:39 +0100

    Neovim on ubuntu

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -2,19 +2,30 @@
 
 set -e
 
+sudo apt install software-properties-common
 sudo apt-add-repository multiverse
 sudo apt-add-repository universe
+sudo apt-add-repository ppa:neovim-ppa/unstable
 sudo apt update
 
 sudo apt install -y \
     fish \
     git \
-    gvim \
     httpie \
     meld \
+    neovim \
     nodejs npm \
     sshuttle \
     tmux \
     xterm
 
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
+
 chsh -s /usr/bin/fish
+
+echo "Open xterm to begin"
