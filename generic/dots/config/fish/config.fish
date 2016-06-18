a58abacbd9867d2a6a0d51811c4f607aa4af885a
Kacper Żuk <kacper.b.zuk@gmail.com>
2016-06-18 21:13:13 +0200

    Tmux fix

--- generic/dots/config/fish/config.fish
+++ generic/dots/config/fish/config.fish
@@ -15,14 +15,10 @@ end
 
 if status --is-interactive
     if [ -z "$TMUX" ]
-        set SESSIONS (tmux list-sessions ^/dev/null)
-        if [ $status -eq 0 ]
-            set SESS_ID (echo $SESSIONS | grep -v "attached" | head -1 | cut -d":" -f1)
-            if [ -n "$SESS_ID" ]
-                echo asd
-                echo $SESS_ID
-                exec tmux -2 -u attach-session -t $SESS_ID
-            end
+        tmux start-server
+        set SESS_ID (tmux list-sessions ^/dev/null | grep -v "attached" | head -1 | cut -d":" -f1)
+        if [ -n "$SESS_ID" ]
+            exec tmux -2 -u attach-session -t $SESS_ID
         end
         exec tmux -2 -u
     end
