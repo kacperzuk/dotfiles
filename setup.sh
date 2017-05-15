356e5e0a436192790c6ede99df5884a643785dae
Kacper Żuk <kacper.b.zuk@gmail.com>
2017-05-15 21:12:57 +0200

    Fixes

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ if [[ -n "$(dirname $0)" ]]; then
     cd "$(dirname $0)"
 fi
 
+[[ -f /etc/arch-release ]] && exec arch/setup.sh $@
+grep -q Ubuntu /etc/lsb-release && exec ubuntu/setup.sh $@
+
 pushd generic >/dev/null
 source ./setup.sh
 popd >/dev/null
-
-[[ -f /etc/arch-release ]] && exec arch/setup.sh $@
-grep -q Ubuntu /etc/lsb-release && exec ubuntu/setup.sh $@
